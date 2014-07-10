d47aa7c562b2b98c31e893eb2e5258787020ede6
Silwest <tyskie3>
2014-07-10 07:23:11 -0500

    NEW: Few small changes

--- mgr/models.py
+++ mgr/models.py
@@ -30,22 +30,23 @@ class FieldOfQuestion(models.Model):
 
 
 class Answer(models.Model):
-    value = models.CharField(max_length=50, blank=False, null=False, default='Put your answer here!')
+    value = models.CharField(max_length=200, blank=False, null=False, default='Put your answer here!')
     correct = models.BooleanField(default=False)
 
     def __unicode__(self):
-        return '{1} - {2}'.format(self.id, self.value)
+        return '%s - %s ' % (self.id, self.value)
 
 
 class Question(models.Model):
+    question_number = models.IntegerField(default=0, blank=False, null=False)
     field_of_question = models.ForeignKey(FieldOfQuestion)
-    value = models.CharField(max_length=200, blank=False, null=False, default='Put you question here!')
+    value = models.CharField(max_length=400, blank=False, null=False, default='Put you question here!')
     image = models.ImageField(upload_to='attachments', blank=True, null=True)
-    answer_1 = models.ForeignKey(Answer, related_name='answer_1')
-    answer_2 = models.ForeignKey(Answer, related_name='answer_2')
-    answer_3 = models.ForeignKey(Answer, related_name='answer_3')
-    answer_4 = models.ForeignKey(Answer, related_name='answer_4')
+    answer_1 = models.ForeignKey(Answer, related_name='answer_1', blank=True, null=True)
+    answer_2 = models.ForeignKey(Answer, related_name='answer_2', blank=True, null=True)
+    answer_3 = models.ForeignKey(Answer, related_name='answer_3', blank=True, null=True)
+    answer_4 = models.ForeignKey(Answer, related_name='answer_4', blank=True, null=True)
     approved_by_admin = models.BooleanField(default=False, blank=True)
 
     def __unicode__(self):
-        return '{1} - {2}'.format(self.field_of_question, self.value)
+        return '%s - %s ' % (self.field_of_question, self.value)
--- mgr/views.py
+++ mgr/views.py
@@ -1,8 +1,11 @@
+import codecs
+import os
+from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
-from mgr.models import Department, FieldOfStudy, FieldOfQuestion
+from mgr.models import Department, FieldOfStudy, FieldOfQuestion, Question, Answer
 
 
 @login_required
@@ -35,3 +38,69 @@ def show_field_of_questions(request, field_of_study_id):
     return render_to_response('mgr/field_of_question.html', {
         'field_of_question': field_of_question,
     }, RequestContext(request))
+
+
+@login_required()
+def work_on_questions(request):
+    questions_without_answer = Question.objects.filter(approved_by_admin=False)
+    count_questions = len(questions_without_answer)
+
+    return render_to_response('mgr/work_on_questions.html', {
+        'questions_without_answer': questions_without_answer,
+        'count_questions': count_questions,
+    }, RequestContext(request))
+
+
+@login_required()
+@staff_member_required
+def load_questions_field_of_question(request, field_of_question_id):
+    field_of_question = FieldOfQuestion.objects.get(id=field_of_question_id)
+    file_path = 'media/attachments/Questions/{}.txt'.format(field_of_question.name)
+    question_answer = []
+    with codecs.open(file_path, encoding='utf-8') as f:
+        while True:
+            question = f.readline()
+            answer = f.readline()
+            question_answer.append((question, answer))
+            if not answer:
+                break  # EOF
+
+    for number, item in enumerate(question_answer):
+        if number == 0:
+            pass
+        else:
+            print item[0], item[1]
+            correct_answer, created = Answer.objects.get_or_create(value=item[1], correct=True)
+            print correct_answer.id
+            Question.objects.get_or_create(question_number=number, field_of_question=field_of_question,
+                                           value=item[0], answer_1=correct_answer)
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
